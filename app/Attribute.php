9d867761b4e4b82b3aeeaab00f0aec74266a66f3
lucky1603 <sinisa.ristic@gmail.com>
2020-07-09 22:48:05 +0200

    Select Attribute.php
    1. Uradjene funkcije za dodavanje i brisanje opcionih vrenodsti.

--- app/Attribute.php
+++ app/Attribute.php
@@ -8,30 +8,58 @@ class Attribute extends Model
 {
     protected $fillable = ['name', 'label', 'type'];
 
+    // ORM things.
+
+    /**
+     * Get entities that contain this attribute.
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
     public function entities() {
         return $this->belongsToMany(Entity::class);
     }
 
+    /**
+     * Get instances that contain this attribute.
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
     public function instances() {
         return $this->belongsToMany(Instance::class)->withPivot('id');
     }
 
+    /**
+     * Get attribute option files, if the attribute is of type 'select'.
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
     public function attribute_options() {
         return $this->hasMany(AttributeOption::class);
     }
 
+    // Methods.
+
+    /**
+     * Gets attribute value in the context of the containing instance.
+     * @return array|mixed|null
+     */
     public function getValue() {
         if(isset($this->pivot)) {
             return Value::get($this->pivot->instance_id, $this);
         }
     }
 
+    /**
+     * Sets the attribute value in the context of the given instance.
+     * @param $value
+     */
     public function setValue($value) {
         if(isset($this->pivot)) {
             Value::put($this->pivot->instance_id, $this, $value);
         }
     }
 
+    /**
+     * Gets the array of option values if the attribute is of 'select' type.
+     * @return array
+     */
     public function getOptions() {
         if($this->attribute_options->count() === 0)
             return [];
@@ -44,6 +72,52 @@ class Attribute extends Model
         return $optionValues;
     }
 
+    /**
+     * Adds the single option.
+     * @param array $optionProperties
+     */
+    public function addOption(Array $optionProperties) {
+        $this->instance->attfibute_options()->save($optionProperties);
+        $this->refresh();
+    }
+
+    /**
+     * Adds the options as bulk.
+     * Sets the options array, reseting the existing content.
+     * @param array $options
+     */
+    public function setOptions(Array $options) {
+
+        // Delete the current options.
+        $this->deleteOptions();
+
+        foreach($options as $key => $value) {
+            $this->attribute_options()->create([
+                'value' => $key,
+                'text' => $value
+            ]);
+        }
+
+        $this->refresh();
+    }
+
+    /**
+     * Delete the current options from the database.
+     */
+    public function deleteOptions() {
+        // Delete existing options.
+        $this->attribute_options->each(function($item, $key) {
+            return $item->delete();
+        });
+
+        $this->refresh();
+    }
+
+    /**
+     * Dumps the previes of all attributes in the database.
+     * @param null $query
+     * @return Attribute[]|\Illuminate\Database\Eloquent\Collection|\Illuminate\Support\Collection
+     */
     public static function dump($query = null) {
         if(!$query) {
             $results = Attribute::all()->map(function($attribute) {
