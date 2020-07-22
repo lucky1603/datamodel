3074e066fc656039f5afcabca46800575f704e8e
lucky1603 <sinisa.ristic@gmail.com>
2020-07-22 21:58:28 +0200

    Promene na datamodelu
    1. Kolona tabele 'datetime_values' promenjena iz 'timestamp' u 'date'.
    2. Za 'bool' dodata opcija za proveru vrednosti i na null.

--- app/Value.php
+++ app/Value.php
@@ -65,7 +65,7 @@ class Value extends Model
 
         $value = $query->value('value');
         if($attribute->type === 'bool') {
-            $value = $value === 0 ? false : true;
+            $value = $value === null || $value === 0 ? false : true;
         }
 
         return $value;
