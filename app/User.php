6d283cdfbc9815ca53beed41f79ee08feac6c84c
lucky1603 <sinisa.ristic@gmail.com>
2020-08-21 19:46:45 +0200

    Dodat je sistem korisnickih rola i aktivnosti.

--- app/User.php
+++ app/User.php
@@ -36,4 +36,31 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * Get assigned roles.
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function roles() {
+        return $this->belongsToMany(Role::class)->withTimestamps();
+    }
+
+    /**
+     * Assign user role.
+     * @param $role
+     */
+    public function assignRole($role) {
+        if(is_string($role)) {
+            $role = Role::whereName($role)->firstOrFail();
+        }
+
+        $this->roles()->sync($role, false);
+    }
+
+    /**
+     * Get all abilities assigned by the attached roles.
+     */
+    public function abilities() {
+        $this->roles->map->abilities->flatten()->pluck('name');
+    }
 }
