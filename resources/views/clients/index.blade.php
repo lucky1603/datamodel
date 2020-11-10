16d399c6ec010695039716bca439ca2b4aeb3572
lucky1603 <sinisa.ristic@gmail.com>
2020-11-10 21:44:15 +0100

    Dohvatanje vrednosti atributa (slika logoa i njegove pozadine u ovom slucaju) uradjeno alternativno, na nacin koji bi trebalo da ima bolje performanse.

--- resources/views/clients/index.blade.php
+++ resources/views/clients/index.blade.php
@@ -5,6 +5,7 @@
     <div class="row" style="padding: 20px; margin-top: 0px;">
         <div class="col-12" style="text-align: center">
             <a href="{{ route("clients.create") }}" title="Dodaj novog klijenta"><img id="addUser" src="/images/adduser.png" class="shadow-lg" style="height: 45px"/></a>
+Y            <hr/>
         </div>
     </div>
     @foreach($clients as $client)
@@ -18,8 +19,8 @@
 
                     <div class="card-body" style="padding: 0" >
                         <div id="img-container" style="position: relative">
-                            <img src="@if( isset($client->getData()['profile_background']) && strlen($client->getData()['profile_background']['filelink']) > 0 ) {{ $client->getData()['profile_background']['filelink'] }} @else '/images/backdefault.jpg' @endif" style="width: 100%"/>
-                            <img class="shadow" src="{{ isset($client->getData()['logo']) && strlen($client->getData()['logo']['filelink']) > 0 ? $client->getData()['logo']['filelink'] : 'images/avatar-default.png' }}" style="width:30%; position:absolute; top:40%; left: 35%;"/>
+                            <img src="@if( $client->getAttribute('profile_background') != null && strlen($client->getAttribute('profile_background')->getValue()['filelink']) > 0 ) {{ $client->getAttribute('profile_background')->getValue()['filelink'] }} @else '/images/backdefault.jpg' @endif" style="width: 100%"/>
+                            <img class="shadow" src="{{ $client->getAttribute('logo') != null && strlen($client->getAttribute('logo')->getValue()['filelink']) > 0 ? $client->getAttribute('logo')->getValue()['filelink'] : 'images/avatar-default.png' }}" style="width:30%; position:absolute; top:40%; left: 35%;"/>
                         </div>
 
                         <h4 style="text-align: center; margin-top: 50px; margin-bottom: 20px"><strong>{{ $client->getData()['name'] }}</strong></h4>
