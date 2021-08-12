9339e3284989908140931188b2660a0774b30427
lucky1603 <sinisa.ristic@gmail.com>
2021-08-12 14:25:55 +0200

    Prepravljena strana za izbor programa.

--- resources/views/profiles/profile.blade.php
+++ resources/views/profiles/profile.blade.php
@@ -1,21 +1,67 @@
 @extends('layouts.hyper-vertical-profile-pre')
 
 @section('profile-content')
-    <div class="card">
+    <div class="card" style="position: absolute; top: 0px; bottom: 0px; right: 0px; left: 0px">
         <div class="card-body">
             @if( in_array($model->getAttribute('profile_status')->getValue(), [1,2]))
-                <h1 class="mb-4">Izaberite jedan od programa ili nas kontaktirajte za više opcija</h1>
-                <ul>
-                    <li>
-                        <a href="{{ route('profiles.apply', ['program' => 5, 'profile' => $model->getId()]) }}">Inkubacija BITF</a>
-                    </li>
-                    <li>
-                        <a href="#">Rastuce kompanije</a>
-                    </li>
-                    <li>
-                        <a href="#">Raising starts</a>
-                    </li>
-                </ul>
+                <h1 class="text-center">Programi</h1>
+                <p class="text-center font-16 font-italic mb-4">Da biste nastavili neophodno je da izaberite jedan od programa.
+                    Ako niste sigurni slobodno nas <a href="mailto://info@ntppark.gov.rs" target="_blank">kontaktirajte</a> za više opcija.</p>
+                <div class="row">
+                    <div class="col-sm-6">
+                        <div class="card">
+                            <div class="row no-gutters align-items-center">
+                                <div class="col-md-4">
+                                    <img src="/images/custom/noimage.png" class="card-img" alt="...">
+                                </div>
+                                <div class="col-md-8">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Inkubacija BITF</h5>
+                                        <p class="card-text">Nešto o programu. Nešto o programu. Nešto o programu. Nešto o programu.
+                                            Nešto o programu. Nešto o programu. Nešto o programu.</p>
+                                        <a href="{{ route('profiles.apply', ['program' => 5, 'profile' => $model->getId()]) }}" class="btn btn-primary">Prijavi se</a>
+                                    </div> <!-- end card-body-->
+                                </div> <!-- end col -->
+                            </div> <!-- end row-->
+                        </div>
+                    </div>
+                    <div class="col-sm-6">
+                        <div class="card">
+                            <div class="row no-gutters align-items-center">
+                                <div class="col-md-4">
+                                    <img src="/images/custom/noimage.png" class="card-img" alt="...">
+                                </div>
+                                <div class="col-md-8">
+                                    <div class="card-body">
+                                        <h5 class="card-title">RASTUĆE KOMPANIJE</h5>
+                                        <p class="card-text">Nešto o programu. Nešto o programu. Nešto o programu. Nešto o programu.
+                                            Nešto o programu. Nešto o programu. Nešto o programu.</p>
+                                        <a href="#" class="btn btn-primary">Prijavi se</a>
+                                    </div> <!-- end card-body-->
+                                </div> <!-- end col -->
+                            </div> <!-- end row-->
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-6">
+                        <div class="card">
+                            <div class="row no-gutters align-items-center">
+                                <div class="col-md-4">
+                                    <img src="/images/custom/noimage.png" class="card-img" alt="...">
+                                </div>
+                                <div class="col-md-8">
+                                    <div class="card-body">
+                                        <h5 class="card-title">RAISING STARTS</h5>
+                                        <p class="card-text">Nešto o programu. Nešto o programu. Nešto o programu. Nešto o programu.
+                                            Nešto o programu. Nešto o programu. Nešto o programu.</p>
+                                        <a href="#" class="btn btn-primary">Prijavi se</a>
+                                    </div> <!-- end card-body-->
+                                </div> <!-- end col -->
+                            </div> <!-- end row-->
+                        </div>
+                    </div>
+                </div>
 
             @else
 
