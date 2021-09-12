548a3fb01f1045cd8d4b1d85f70f0e9e954d7e51
lucky1603 <sinisa.ristic@gmail.com>
2021-09-13 00:09:33 +0200

    Intaliran Bootsrap Vue.

--- resources/js/components/SessionEditor.vue
+++ resources/js/components/SessionEditor.vue
@@ -13,6 +13,9 @@
                     SESSION DETAILS
                 </span>
             </div>
+            <div class="card-body">
+                <b-table striped hover :items="items" sticky-header="250px" small select-mode="single"></b-table>
+            </div>
         </div>
     </div>
 
@@ -30,7 +33,17 @@ export default {
             programs: [],
             sessions: [],
             selectedProgram: {type: Object, default: null },
-            selectedSession: {type: Object, default: null }
+            selectedSession: {type: Object, default: null },
+            items: [
+                { age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
+                { age: 21, first_name: 'Larsen', last_name: 'Shaw' },
+                { age: 89, first_name: 'Geneva', last_name: 'Wilson' },
+                { age: 38, first_name: 'Jami', last_name: 'Carney' },
+                { age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
+                { age: 21, first_name: 'Larsen', last_name: 'Shaw' },
+                { age: 89, first_name: 'Geneva', last_name: 'Wilson' },
+                { age: 38, first_name: 'Jami', last_name: 'Carney' }
+            ]
         }
     },
     mounted() {
--- resources/js/vue.js
+++ resources/js/vue.js
@@ -5,7 +5,16 @@
  */
 
 import Vue from "vue"
+import {BootstrapVue, IconsPlugin} from "bootstrap-vue";
+
+// import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 window.Vue = require('vue');
+
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
+
 
 /**
  * The following block of code may be used to automatically register your
