0c56309be1961f2b65fba1027978a9fc13ad1ca0
lucky1603 <sinisa.ristic@gmail.com>
2021-11-11 08:19:20 +0100

    Prilagodjena strana za prijavu.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -14,6 +14,7 @@
     <link href="/assets/css/icons.min.css" rel="stylesheet" type="text/css" />
     <link href="/assets/css/app.min.css" rel="stylesheet" type="text/css" id="light-style" />
     <link href="/assets/css/app-dark.min.css" rel="stylesheet" type="text/css" id="dark-style" />
+    <link href="/css/my.css" rel="stylesheet" type="text/css" />
 
 </head>
 
@@ -36,7 +37,7 @@
                 </div>
 
                 <!-- title-->
-                <h4 class="mt-3">{{ __('Sign In') }}</h4>
+                <h4 class="mt-3 text-center mb-2">{{ __('Sign In') }}</h4>
                 <p class="text-muted mb-4">{{ __('auth.sign_in_message') }}</p>
 
                 <!-- form -->
@@ -73,24 +74,24 @@
                     <div class="form-group mb-0 text-center">
                         <button class="btn btn-primary btn-block" type="submit"><i class="mdi mdi-login mr-1"></i>{{ __('Log in') }}</button>
                     </div>
-                    <!-- social-->
-                    <div class="text-center mt-4">
-                        <p class="text-muted font-16">{{ __('Sign in with') }}</p>
-                        <ul class="social-list list-inline mt-3">
-                            <li class="list-inline-item">
-                                <a href="javascript: void(0);" class="social-list-item border-primary text-primary"><i class="mdi mdi-facebook"></i></a>
-                            </li>
-                            <li class="list-inline-item">
-                                <a href="javascript: void(0);" class="social-list-item border-danger text-danger"><i class="mdi mdi-google"></i></a>
-                            </li>
-                            <li class="list-inline-item">
-                                <a href="javascript: void(0);" class="social-list-item border-info text-info"><i class="mdi mdi-twitter"></i></a>
-                            </li>
-                            <li class="list-inline-item">
-                                <a href="javascript: void(0);" class="social-list-item border-secondary text-secondary"><i class="mdi mdi-github-circle"></i></a>
-                            </li>
-                        </ul>
-                    </div>
+{{--                    <!-- social-->--}}
+{{--                    <div class="text-center mt-4">--}}
+{{--                        <p class="text-muted font-16">{{ __('Sign in with') }}</p>--}}
+{{--                        <ul class="social-list list-inline mt-3">--}}
+{{--                            <li class="list-inline-item">--}}
+{{--                                <a href="javascript: void(0);" class="social-list-item border-primary text-primary"><i class="mdi mdi-facebook"></i></a>--}}
+{{--                            </li>--}}
+{{--                            <li class="list-inline-item">--}}
+{{--                                <a href="javascript: void(0);" class="social-list-item border-danger text-danger"><i class="mdi mdi-google"></i></a>--}}
+{{--                            </li>--}}
+{{--                            <li class="list-inline-item">--}}
+{{--                                <a href="javascript: void(0);" class="social-list-item border-info text-info"><i class="mdi mdi-twitter"></i></a>--}}
+{{--                            </li>--}}
+{{--                            <li class="list-inline-item">--}}
+{{--                                <a href="javascript: void(0);" class="social-list-item border-secondary text-secondary"><i class="mdi mdi-github-circle"></i></a>--}}
+{{--                            </li>--}}
+{{--                        </ul>--}}
+{{--                    </div>--}}
                 </form>
                 <!-- end form-->
 
@@ -107,12 +108,12 @@
     <!-- Auth fluid right content -->
     <div class="auth-fluid-right text-center">
         <div class="auth-user-testimonial">
-            <h2 class="mb-3">I love the color!</h2>
-            <p class="lead"><i class="mdi mdi-format-quote-open"></i> It's a elegent templete. I love it very much! . <i class="mdi mdi-format-quote-close"></i>
-            </p>
-            <p>
-                - Hyper Admin User
-            </p>
+{{--            <h2 class="mb-3">I love the color!</h2>--}}
+{{--            <p class="lead"><i class="mdi mdi-format-quote-open"></i> It's a elegent templete. I love it very much! . <i class="mdi mdi-format-quote-close"></i>--}}
+{{--            </p>--}}
+{{--            <p>--}}
+{{--                - Hyper Admin User--}}
+{{--            </p>--}}
         </div> <!-- end auth-user-testimonial-->
     </div>
     <!-- end Auth fluid right content -->
