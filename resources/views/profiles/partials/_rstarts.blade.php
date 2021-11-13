4bc4107f983ebfa51fbf868232f4812379bd9602
lucky1603 <sinisa.ristic@gmail.com>
2021-11-13 16:36:06 +0100

    Malo smo dali senku i formatirali head forme.

--- resources/views/profiles/partials/_rstarts.blade.php
+++ resources/views/profiles/partials/_rstarts.blade.php
@@ -4,7 +4,7 @@
 <div class="text-center mt-4 mb-4">
     <h1 class="attribute-label">PRIJAVA</h1>
 </div>
-<div class="bg-light attribute-label mb-2 pt-2" style="display: flex; align-items: center; justify-content: center; flex-direction: column;">
+<div class="bg-light attribute-label font-14 mb-2 p-4 shadow" style="display: flex; align-items: center; justify-content: center; flex-direction: column;">
     <p>U cilju boljeg razumevanja Prijavnog formulara, obe kategorije se u daljem tekstu nazivaju “startap”.</p>
     <p>Obavezna polja su označena zvezdicom (<span class="text-danger">*</span>) i različitom pozadinom.</p>
 </div>
