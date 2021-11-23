63c1e825bec314efc7d8bdbbcaed443b83074c1b
lucky1603 <sinisa.ristic@gmail.com>
2021-11-23 01:12:19 +0100

    Uradjena validacija telefona.

--- resources/views/profiles/partials/_rstarts_general_data.blade.php
+++ resources/views/profiles/partials/_rstarts_general_data.blade.php
@@ -88,6 +88,7 @@
         <input type="text"
                class="form-control form-control-sm @error($attribute->name) is-invalid @enderror mandatory-field"
                id="{{ $attribute->name }}" name="{{ $attribute->name }}" value="{{ $value }}" @if(isset($model)) disabled @endif>
+        <span class="font-12 text-dark">Unesite broj telefona u formatu 0(##) ###(#) - ###(#)</span>
         @error($attribute->name)
             <div class="alert alert-danger">{{ $message }}</div>
         @enderror
