6a0f27501c2a9292de788be76193109115d1092f
lucky1603 <sinisa.ristic@gmail.com>
2021-11-23 08:12:40 +0100

    Prevedena validaciona poruka kada je u pitanju regex.

--- resources/lang/sr-RS/validation.php
+++ resources/lang/sr-RS/validation.php
@@ -95,7 +95,7 @@ return [
     'numeric' => 'Ovo polje mora biti broj.',
     'password' => 'The password is incorrect.',
     'present' => 'The :attribute field must be present.',
-    'regex' => 'The :attribute format is invalid.',
+    'regex' => 'Neispravan format podatka.',
     'required' => 'Ovo je polje obavezno.',
     'required_if' => 'The :attribute field is required when :other is :value.',
     'required_unless' => 'The :attribute field is required unless :other is in :values.',
