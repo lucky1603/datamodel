71ed90b3c700fd54b0d613608decd48c830d7024
lucky1603 <sinisa.ristic@gmail.com>
2022-02-25 15:42:21 +0100

    Uradjena je validacija podataka prilikom dodavanja korisnika.

--- resources/views/auth/addforprofile.blade.php
+++ resources/views/auth/addforprofile.blade.php
@@ -2,12 +2,12 @@
 
 @section('content')
     <h1>{{__('Add New User')}} {{ __('for profile') }} - {{ $profile->getData()['name'] }}</h1>
-    <form method="POST" enctype="multipart/form-data" action="{{ route('user.addedforprofile', $profile->getId()) }}">
+    <form method="POST" enctype="multipart/form-data" action="{{ route('user.addedforprofile', $profile->getId()) }}" id="addUserForm">
         @csrf
         @if(isset($backroute))
             <input type="hidden" name="backroute" id="backroute" value="{{ $backroute }}">
         @endif
-        @include('forms.adduser')
+        @include('forms.adduser', ['akcija' => route('user.addedforprofile', $profile->getId())])
     </form>
 @endsection
 
--- resources/views/forms/adduser.blade.php
+++ resources/views/forms/adduser.blade.php
@@ -10,45 +10,90 @@
     <div class="col-md-8">
         <div class="form-group">
             <label for="name">{{ __('Name') }}</label>
-            <input type="text" id="name" name="name" class="form-control" required>
+            <input type="text" id="name" name="name" class="form-control">
+            <span class="errText text-danger" id="errName"></span>
         </div>
         <div class="form-group">
             <label for="email">{{ __('E-Mail') }}</label>
-            <input type="text" id="email" name="email" class="form-control" required>
+            <input type="text" id="email" name="email" class="form-control">
+            <span class="errText text-danger" id="errEmail"></span>
         </div>
 
         <div class="form-group">
             <label for="password">{{ __('Password') }} ({{ __('8 characters minimum') }})</label>
-            <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-            @error('password')
-            <span class="invalid-feedback" role="alert">
-                        <strong>{{ $message }}</strong>
-                    </span>
-            @enderror
+            <input id="password" type="password" class="form-control"  name="password" autocomplete="new-password">
         </div>
 
-        <div class="form-group">
+        <div class="form-group mb-0">
             <label for="password-confirm">{{ __('Confirm Password') }}</label>
-            <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+            <input id="password-confirm" type="password" class="form-control" name="password_confirmation" autocomplete="new-password">
         </div>
+        <span class="errText text-danger" id="errPassword"></span>
 
-        <div class="form-group">
+        <div class="form-group mb-0">
             <label for="position">{{ __('Position') }}</label>
             <input type="text" id="position" name="position" class="form-control">
         </div>
+        <span class="errText text-danger mt-0" id="errPosition"></span>
     </div>
 </div>
 
 <hr/>
 
 <div class="text-center">
-    <button type="submit" class="btn btn-sm btn-primary">{{ __('Save') }}</button>
+    <button type="button" id="submitButton" class="btn btn-sm btn-primary">{{ __('Save') }}</button>
 </div>
+
+<style>
+    .errText {
+        margin: 0px;
+        font-size: 12px;
+    }
+</style>
 
 <script type="text/javascript">
     $('#textBtn').click(function() {
         $('#photo').trigger('click');
-
     })
+
+    $('#submitButton').click(function() {
+        const message = "<?php echo $akcija; ?>"
+        let formData = new FormData($('form#addUserForm')[0]);
+        axios.post(message, formData)
+        .then(response => {
+            $('.errText').text('').hide();
+            $('.dialogHost').hide();
+            location.reload();
+        })
+        .catch(error => {
+            let errors = error.response.data.errors;
+            if(errors.name != undefined) {
+                $('#errName').show();
+                $('#errName').text(errors.name);
+            } else {
+                $('#errName').hide();
+            }
+
+            if(errors.email != undefined) {
+                $('#errEmail').show();
+                $('#errEmail').text(errors.email);
+            } else {
+                $('#errEmail').hide();
+            }
+
+            if(errors.password != undefined) {
+                $('#errPassword').show().text(errors.password);
+            } else {
+                $('#errPassword').hide();
+            }
+
+            if(errors.position != undefined) {
+                $('#errPosition').show().text(errors.position);
+            } else {
+                $('#errPosition').hide();
+            }
+
+        });
+    })
+
 </script>
