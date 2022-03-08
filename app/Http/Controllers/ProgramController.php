1faf4ca4183cd93bd6cb23aaa90a00129cef2067
lucky1603 <sinisa.ristic@gmail.com>
2022-03-08 23:08:15 +0100

    Prvi uspeh sa podesavanjem i ucitavanjem podataka forme za statistiku.

---
+++ app/Http/Controllers/ProgramController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Business\Program;
+use Illuminate\Http\Request;
+
+class ProgramController extends Controller
+{
+    public function getStatistics($programId): array
+    {
+        $program = Program::find($programId);
+        return [
+            'iznos_prihoda' => $program->getValue('iznos_prihoda'),
+            'iznos_izvoza' => $program->getValue('iznos_izvoza'),
+            'broj_zaposlenih' => $program->getValue('broj_zaposlenih'),
+            'broj_angazovanih' => $program->getValue('broj_angazovanih'),
+            'broj_angazovanih_zena' => $program->getValue('broj_angazovanih_zena'),
+            'iznos_placenih_poreza' => $program->getValue('iznos_placenih_poreza'),
+            'iznos_ulaganja_istrazivanje_razvoj' => $program->getValue('iznos_ulaganja_istrazivanje_razvoj'),
+            'broj_malih_patenata' => $program->getValue('broj_malih_patenata'),
+            'broj_patenata' => $program->getValue('broj_patenata'),
+            'broj_autorskih_dela' => $program->getValue('broj_autorskih_dela'),
+            'broj_inovacija' => $program->getValue('broj_inovacija'),
+            'countries' => $program->getValue('countries'),
+        ];
+    }
+
+    public function updateStatistics(Request $request) {
+        $data = $request->post();
+
+        $program = Program::find($data['id']);
+        foreach($data as $key=>$value) {
+            $program->setValue($key, $value);
+        }
+
+    }
+}
---
+++ resources/js/components/CountriesSelector.vue
@@ -0,0 +1,51 @@
+<template>
+   <item-selector
+       v-model="value"
+       ref="selector"
+       :originalitems="originalItems"
+       left_title="SVE ZEMLJE"
+       right_title="IZABRANE ZEMLJE"
+       @input="selectionChanged"></item-selector>
+</template>
+
+<script>
+export default {
+    name: "CountriesSelector",
+    props: {
+        value: [],
+        source: { typeof: String, default: '/analytics/countries' }
+    },
+    methods: {
+        async getData() {
+            let countries = [];
+            await axios.get(this.source)
+            .then(response => {
+                this.originalItems.length = 0;
+                countries = response.data;
+
+                countries.forEach(country => {
+                    let item = {value: country.id, text: country.name, selected: false};
+                    if(this.value.includes(item.value)) {
+                        item.selected = true;
+                    }
+                    this.originalItems.push(item);
+                });
+            });
+
+        },
+        selectionChanged() {
+            console.log('selection changed!!!!');
+            this.$emit('input', this.value);
+        }
+    },
+    mounted() {
+        setTimeout(this.getData, 1000);
+    },
+    data() {
+        return {
+            originalItems: [],
+            selectedItems: this.value
+        }
+    }
+}
+</script>
