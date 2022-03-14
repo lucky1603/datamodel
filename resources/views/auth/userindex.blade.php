5fda69c67559a79624da9afa46ef43e214f7fba4
lucky1603 <sinisa.ristic@gmail.com>
2022-03-14 15:31:20 +0100

    Ubrzano prikazivanje korisnika.

--- resources/views/auth/userindex.blade.php
+++ resources/views/auth/userindex.blade.php
@@ -33,21 +33,27 @@
     </table>
 
     @foreach($profiles as $profile)
-
         <h5 class="mt-2 p-1 user-group-header" style="display: flex">
-            <span class="float-left flex-grow-1">{{ $profile->getData()['name'] }}</span>
-            <a href="{{ route('user.addforprofile', $profile->getId()) }}" class="text-light edituser" data-toggle="modal" data-target="#dialogHost"><i class="mdi mdi-text-account float-right flex-grow-0"></i></a>
+            <span class="float-left flex-grow-1">{{ $profile->getValue('name') }}</span>
+            <a href="{{ route('user.addforprofile', $profile->instance->id) }}" class="text-light edituser" data-toggle="modal" data-target="#dialogHost"><i class="mdi mdi-text-account float-right flex-grow-0"></i></a>
         </h5>
-
-
         <table class="table table-striped">
-            @foreach($profile->getUsers() as $user)
+            @php
+                $users = $profile->getUsers();
+            @endphp
+            @foreach($users as $user)
                 <tr>
-                    <td width="10%"><img src="{{ $user->photo }}" class="rounded-circle" width="24" height="24"></td>
-                    <td width="20%">{{ $user->name }}</td>
-                    <td width="20%">{{ $user->email }}</td>
-                    <td width="40%">{{ $user->position }}</td>
-                    <td width="10%">
+                    <td style="width: 10%">
+                        @if($user->photo == null && $user->photo != ['filelink' => '', 'filename' => ''])
+                            <img src=" /images/custom/nophoto2.png" class="rounded-circle" width="24" height="24">
+                        @else
+                            <img src="{{ $user->photo }}" class="rounded-circle" width="24" height="24">
+                        @endif
+                    </td>
+                    <td style="width: 20%">{{ $user->name }}</td>
+                    <td style="width: 20%">{{ $user->email }}</td>
+                    <td style="width: 40%">{{ $user->position }}</td>
+                    <td style="width: 10%">
                         <a href="{{ route('user.edit', $user->id) }}" class="edituser" data-toggle="modal" data-target="#dialogHost"><i class="mdi mdi-pencil mr-2"></i></a>
                         <a href="{{ route('user.delete', $user->id) }}" class="edituser" data-toggle="modal" data-target="#dialogHost"><i class="mdi mdi-delete mr-2"></i></a>
                     </td>
