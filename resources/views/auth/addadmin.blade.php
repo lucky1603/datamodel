61dcdb1017337bcbf7b1c2e24a841a666fb48f72
lucky1603 <sinisa.ristic@gmail.com>
2022-08-30 14:36:13 +0200

    Moze da se dodaje admin ponovo.

--- resources/views/auth/addadmin.blade.php
+++ resources/views/auth/addadmin.blade.php
@@ -2,8 +2,11 @@
 
 @section('content')
     <h1>{{__('Add New Admin')}}</h1>
-    <form method="POST" enctype="multipart/form-data" action="{{ route('user.adminadded') }}">
+    <form method="POST" id="addUserForm" enctype="multipart/form-data" action="{{ route('user.adminadded') }}">
         @csrf
+        @php
+            $akcija = '/edituser/adminadded';
+        @endphp
         @include('forms.adduser')
     </form>
 @endsection
