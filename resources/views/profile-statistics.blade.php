cfab371dbaf4bd314c2558608779a9342b93b853
lucky1603 <sinisa.ristic@gmail.com>
2022-10-06 23:03:49 +0200

    mala popravka

--- resources/views/profile-statistics.blade.php
+++ resources/views/profile-statistics.blade.php
@@ -2,7 +2,7 @@
 
 @section('page-header')
     <div class="w-50 d-inline-block" style="height: 7vh">
-        <div><span class="h4" style="position: relative; top:2vh; left: 1vh">{{ mb_strtoupper(_('Statistics'))}} - {{ mb_strtoupper(_('Companies'))}}</span></div>
+        <div><span class="h4" style="position: relative; top:2vh; left: 1vh">{{ mb_strtoupper(__('Statistics'))}} - {{ mb_strtoupper(__('Companies'))}}</span></div>
     </div>
 @endsection
 
