47228e0589bfb55d1d4071383aa3ab4a6b0bda10
lucky1603 <sinisa.ristic@gmail.com>
2022-10-07 18:38:29 +0200

    Popravka glavne strane.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,8 +1,18 @@
 @extends('layouts.hyper-vertical-mainframe')
 
+@php
+    $locale = session('locale');
+    if($locale == null) {
+        $locale = app()->getLocale();
+    } else {
+        app()->setLocale($locale);
+    }
+
+@endphp
+
 @section('page-header')
     <div class="w-50 d-inline-block" style="height: 7vh">
-        <div><span class="h4" style="position: relative; top:2vh; left: 1vh">{{ mb_strtoupper(__('Statistics'))  }} - RAISING STARTS</span></div>
+        <div><span class="h4" style="position: relative; top:2vh; left: 1vh">{{ mb_strtoupper( __('Statistics') )}} - RAISING STARTS</span></div>
 
     </div>
 @endsection
--- resources/views/profile-statistics.blade.php
+++ resources/views/profile-statistics.blade.php
@@ -1,5 +1,15 @@
 @extends('layouts.hyper-vertical-mainframe')
 
+@php
+    $locale = session('locale');
+    if($locale == null) {
+        $locale = app()->getLocale();
+    } else {
+        app()->setLocale($locale);
+    }
+
+@endphp
+
 @section('page-header')
     <div class="w-50 d-inline-block" style="height: 7vh">
         <div><span class="h4" style="position: relative; top:2vh; left: 1vh">{{ mb_strtoupper(__('Statistics'))}} - {{ mb_strtoupper(__('Companies'))}}</span></div>
