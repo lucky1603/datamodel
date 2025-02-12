05538f82cb86f31e7690ba53f46c1c68a8dc2d37
Sinisa Ristic <sinisa.ristic@gmail.com>
2025-02-13 00:23:12 +0100

    Napravljena je kontrola za otvaranje i zatvaranje javnog poziva.

---
+++ resources/js/components/PublicCallManager.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <b-form @submit.prevent="sendData">
+        <b-form-checkbox
+            id="checkbox-1"
+            name="checkbox-1"
+            :value="true"
+            :unchecked-value="false"
+            v-model="form.unlock"
+        >
+            {{ _('gui.unlock_public_call') }}
+        </b-form-checkbox>
+        <b-form-group 
+            v-if="form.unlock == true"
+            :label="_('gui.public_call_start')" 
+            label-cols="3"
+            content-cols="9"
+            label-class="text-primary"
+            :description="_('gui.public_call_start_text')"  class="mt-3">
+            <b-form-input type="datetime-local" v-model="form.start"></b-form-input>
+        </b-form-group>
+        <b-form-group 
+            v-if="form.unlock == true"
+            label-cols="3"
+            content-cols="9"
+            :label="_('gui.public_call_end')" 
+            label-class="text-primary"
+            :description="_('gui.public_call_end_text')" class="mt-3">
+            <b-form-input type="datetime-local" v-model="form.end"></b-form-input>
+        </b-form-group>
+    </b-form>   
+  </div>
+</template>
+<script>
+import _ from 'lodash';
+
+export default {
+  name: 'PublicCallManager',
+
+  data() {
+    return {
+        form: {
+            unlock: false,
+            start: null,
+            end: null
+        }
+    };
+  },
+
+  mounted() {
+    
+  },
+
+  methods: {
+    
+  },
+};
+</script>
